bb8343750625b958ebc0b6560ce8659590f73c27
Maurice <ombewamaurice79@gmail.com>
2025-11-04 12:30:50 +0300

    Initial commit: Basic Angular components structure with header, body, and footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,39 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  selector: 'header',
+  templateUrl: './header.html',
+  // styleUrl: './app.css'
+})
+export class Nav {
+  protected readonly title = signal('project2');
+}
+
+
+@Component({
+  selector: 'body',
+  templateUrl: './body.html',
+  styleUrl: './app.css'
+})
+export class Body {
+  protected readonly title = signal('project2');
+}
+
+
+@Component({
+  selector: 'footer',
+  templateUrl: './footer.html',
+  styleUrl: './app.css'
+})
+export class Footer {
+  protected readonly title = signal('project2');
+}
+
+
+@Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
+  imports: [Nav, Body, Footer],
   styleUrl: './app.css'
 })
 export class App {
